9b0d19eb0cbe4a507d417a5796fabb6a8f3d2614
karansasan4113 <karansasan4113@gmail.com>
2024-02-05 15:34:07 +0530

    qw

--- menu-bar.js
+++ menu-bar.js
@@ -1,31 +1,22 @@
-function toggleSidebar(event) {
-    var body = document.body;
+function toggleSidebar() {
+  
     var sidebar = document.getElementById('sidebar');
     var overlay = document.querySelector('.overlay');
 
-    if (event) {
-        event.preventDefault(); // Prevent the default behavior (e.g., scrolling to the top)
-        event.stopPropagation(); // Stop the event from propagating to parent elements
-    }
-  
-    body.classList.toggle('sidebar-active');
+   
     sidebar.classList.toggle('active');
     overlay.classList.toggle('active');
 }
 
-function closeSidebar(event) {
-    var body = document.body;
+function closeSidebar() {
+  
     var sidebar = document.getElementById('sidebar');
     var overlay = document.querySelector('.overlay');
 
-    if (event) {
-        event.preventDefault(); // Prevent the default behavior (e.g., scrolling to the top)
-    }
-
-    body.classList.remove('sidebar-active');
     sidebar.classList.remove('active');
     overlay.classList.remove('active');
 }
+
 
 
 function changeStylesheet(href) {
